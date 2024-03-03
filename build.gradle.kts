2c45ed6ed2f2f65a4e2c2221659643611d23244b
seoyoon <syoonlee513@gmail.com>
2024-03-04 00:13:07 +0900

    [chore] root gradle에 전체 모듈 공통 적용사항 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.android.library) apply false
 }
