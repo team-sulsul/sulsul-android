489c82b0d521cc1aac540f39adf63b406e525f39
seoyoon <syoonlee513@gmail.com>
2024-03-04 04:42:10 +0900

    [chore] ktlint, detekt 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,29 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.detekt)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.android.library) apply false
+}
+
+allprojects {
+    val projectPath = rootProject.file(".").absolutePath
+
+    apply {
+        plugin(rootProject.libs.plugins.detekt.get().pluginId)
+        plugin(rootProject.libs.plugins.ktlint.get().pluginId)
+    }
+
+    afterEvaluate {
+        detekt {
+            parallel = true
+            buildUponDefaultConfig = true
+            toolVersion = libs.versions.detekt.get()
+            config.setFrom(files("$rootDir/detekt-config.yml"))
+        }
+    }
 }
