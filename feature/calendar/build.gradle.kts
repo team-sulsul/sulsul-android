83044958e4e8b851ea7738f5ee16c939f704dc34
seoyoon <syoonlee513@gmail.com>
2024-03-06 14:38:56 +0900

    [chore] :feature 모듈에 :calendar, :report, :setting 모듈 추가

---
+++ feature/calendar/build.gradle.kts
@@ -0,0 +1,18 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("sulsul.android.feature")
+}
+
+android {
+    namespace = "com.sulsul.feature.calendar"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
