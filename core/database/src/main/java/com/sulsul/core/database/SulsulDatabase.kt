5fd43e627e2d7e6eb4ee11ca7b249238ca93edac
seoyoon <syoonlee513@gmail.com>
2024-03-31 22:34:15 +0900

    [feat] LocalDate TypeConverter 추가

--- core/database/src/main/java/com/sulsul/core/database/SulsulDatabase.kt
+++ core/database/src/main/java/com/sulsul/core/database/SulsulDatabase.kt
@@ -2,10 +2,12 @@ package com.sulsul.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.sulsul.core.database.dao.DrinkInfoDao
 import com.sulsul.core.database.dao.DrinkRecordDao
 import com.sulsul.core.database.model.DrinkInfoEntity
 import com.sulsul.core.database.model.DrinkRecordEntity
+import com.sulsul.core.database.util.Converters
 
 @Database(
     entities = [
@@ -14,6 +16,7 @@ import com.sulsul.core.database.model.DrinkRecordEntity
     ],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class SulsulDatabase : RoomDatabase() {
     abstract fun drinkRecordDao(): DrinkRecordDao
     abstract fun drinkInfoDao(): DrinkInfoDao
