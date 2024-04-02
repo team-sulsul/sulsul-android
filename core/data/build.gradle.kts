b9c1a8df212345f89cec9b0560219c04ad374ab0
안정은 <jjeee1313@gmail.com>
2024-04-02 23:55:37 +0900

    [feat] ApiModule 추가

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("sulsul.android.library")
     id("sulsul.android.hilt")
     id("sulsul.kotlin.hilt")
+    id("kotlinx-serialization")
 }
 
 android {
@@ -12,4 +13,13 @@ android {
 dependencies {
     implementation(projects.core.model)
     implementation(projects.core.database)
+
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.datetime)
+    testImplementation(libs.turbine)
+
+    implementation(libs.timber)
 }
