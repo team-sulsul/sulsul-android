36c7865fafc9d207239601d414d33fc4292ad84d
seoyoon <syoonlee513@gmail.com>
2024-04-03 20:17:31 +0900

    [feat] 캘린더 -> 술 선택 화면으로 데이터 넘기기 위한 safeArgs 세팅

--- core/model/src/main/java/com/sulsul/core/model/DrinkRecord.kt
+++ core/model/src/main/java/com/sulsul/core/model/DrinkRecord.kt
@@ -1,10 +1,13 @@
 package com.sulsul.core.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
+@Parcelize
 data class DrinkRecord(
     val id: Int = 0,
-    val recordedAt: LocalDate,
-    val drunkennessLevel: String,
-    val drinks: List<DrinkInfo>
-)
+    val recordedAt: LocalDate = LocalDate.now(),
+    val drunkennessLevel: String = "",
+    val drinks: List<DrinkInfo> = emptyList()
+) : Parcelable
