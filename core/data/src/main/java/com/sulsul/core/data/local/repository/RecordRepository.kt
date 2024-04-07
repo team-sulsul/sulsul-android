07d0605263ba5bdffe614c4f58c87011ddd9735b
seoyoon <syoonlee513@gmail.com>
2024-04-07 13:27:15 +0900

    [feat] repository 패키지 구조, 이름 변경 및 기능 구현

--- core/data/src/main/java/com/sulsul/core/data/local/repository/RecordRepository.kt
+++ core/data/src/main/java/com/sulsul/core/data/local/repository/RecordRepository.kt
@@ -1,4 +1,4 @@
-package com.sulsul.core.data.repository.local
+package com.sulsul.core.data.local.repository
 
 import android.util.Log
 import com.sulsul.core.database.dao.DrinkInfoDao
@@ -10,9 +10,10 @@ import com.sulsul.core.model.DrinkRecord
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
+import java.time.LocalDate
 import javax.inject.Inject
 
-class DrinkRecordRepository @Inject constructor(
+class RecordRepository @Inject constructor(
     private val recordDao: DrinkRecordDao,
     private val drinkInfoDao: DrinkInfoDao,
 ) {
@@ -25,6 +26,28 @@ class DrinkRecordRepository @Inject constructor(
         }
     }
 
+    suspend fun insertRecord(record: DrinkRecord) {
+        val recordId = recordDao.insertRecord(record = record.asEntity()) // 외래키 id 생성
+        insertDrinks(recordId, record.drinks)
+    }
+
+    suspend fun updateDrunkennessLevel(date: LocalDate, state: String) {
+        val drinkRecordEntity = recordDao.getRecordByDate(date) // 날짜에 해당하는 entity를 가져온다
+        drinkRecordEntity.let {
+            it.drunkennessLevel = state // level값만 변경하여
+            recordDao.updateRecord(drinkRecordEntity) // 업데이트 한다 (id 기준 업데이트)
+        }
+    }
+
+    private suspend fun insertDrinks(recordId: Long, drinks: List<DrinkInfo>) {
+        drinks.forEach { drink ->
+            val drinkInfoEntity = drink.asEntity(recordId = recordId.toInt())
+            drinkInfoDao.insertDrinkInfo(drinkInfo = drinkInfoEntity)
+            Log.d("술 데이터 저장", "$recordId, $drinkInfoEntity")
+
+        }
+    }
+
     private fun getDrinkInfoList(recordId: Int): Flow<List<DrinkInfo>> =
         drinkInfoDao.getDrinkInfoListByRecordId(recordId).map { infoList ->
             infoList.map {
@@ -32,16 +55,4 @@ class DrinkRecordRepository @Inject constructor(
             }
         }
 
-    suspend fun addDrinkRecordWithDrinkInfo(record: DrinkRecord) {
-        val recordId = recordDao.insertRecord(record = record.asEntity()) // 외래키 id 생성
-        addDrinkInfo(recordId, record.drinks)
-    }
-
-    private suspend fun addDrinkInfo(recordId: Long, drinkInfoList: List<DrinkInfo>) {
-        drinkInfoList.forEach { info ->
-            val drinkInfoEntity = info.asEntity(recordId.toInt()) // 외래키 설정하여 DrinkInfoEntity 반환
-            drinkInfoDao.insertDrinkInfo(drinkInfo = drinkInfoEntity) // DrinkInfoEntity 삽입
-            Log.d("술 데이터 저장", "$recordId, $drinkInfoEntity")
-        }
-    }
 }
