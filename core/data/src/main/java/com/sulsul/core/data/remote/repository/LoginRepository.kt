5ca8cc5b7124b324f09643b8c35a8c2077d1015f
안정은 <jjeee1313@gmail.com>
2024-04-16 22:40:47 +0900

    [chore] 로그인 repository interface, repositoryModule 삭제

--- core/data/src/main/java/com/sulsul/core/data/remote/repository/LoginRepository.kt
+++ core/data/src/main/java/com/sulsul/core/data/remote/repository/LoginRepository.kt
@@ -1,8 +1,15 @@
 package com.sulsul.core.data.remote.repository
 
+import com.sulsul.core.data.api.LoginApi
 import com.sulsul.core.data.remote.model.request.LoginRequest
 import com.sulsul.core.data.remote.model.response.LoginResponse
+import javax.inject.Inject
 
-interface LoginRepository {
-    suspend fun postLogin(kakaoAccess: LoginRequest): LoginResponse
+
+class LoginRepository @Inject constructor(
+    private val loginApi: LoginApi
+) {
+    suspend fun postLogin(kakaoAccess: LoginRequest): LoginResponse {
+        return loginApi.postLogin(kakaoAccess)
+    }
 }
