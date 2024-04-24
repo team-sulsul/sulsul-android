26ca4af3fdffa1fd88c5034ac3fb380a5db61e2d
안정은 <jjeee1313@gmail.com>
2024-04-24 20:46:16 +0900

    [feat] 로그인 실패 예외처리 추가, 로그인 결과 토스트 발생

--- feature/login/src/main/java/com/sulsul/feature/login/viewModel/LoginViewModel.kt
+++ feature/login/src/main/java/com/sulsul/feature/login/viewModel/LoginViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,22 +17,27 @@ class LoginViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val success = "200"
-    var errorMsg = ""
+    private val TAG = "login"
 
     private val _loinSuccess = MutableStateFlow<Boolean>(false)
     val loginSuccess: StateFlow<Boolean> = _loinSuccess
+
+    private val _errorMsg = MutableStateFlow<String>("")
+    val errorMsg: StateFlow<String> = _errorMsg
 
     // TODO : 서버 측에서 응답 구조, 에러코드 상세화 변경되면 코드 변경 필요
     fun postLogin(kakaoAccess: String) {
         viewModelScope.launch {
             loginUseCase.tryLogin(kakaoAccess)
                 .catch {
+                    _errorMsg.value = "failed"
+                    Timber.tag(TAG).d(it)
                 }.collect {
                     if (it.message == success) {
                         _loinSuccess.value = true
                     } else {
-                        _loinSuccess.value = false
-                        errorMsg = "failed" // 서버에서 fail에 대한 응답을 줘야 수정할 수 있을듯..?
+                        _errorMsg.value = "failed" // 서버에서 fail에 대한 응답을 줘야 수정할 수 있을듯..?
+                        Timber.tag(TAG).d(it.message)
                     }
                 }
         }
