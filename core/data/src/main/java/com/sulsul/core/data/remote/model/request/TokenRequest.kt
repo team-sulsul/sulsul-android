2bc5291e1263bcb20d76d2e7b0c734f15905a1b0
jeongeun <jeongsilver@wjtb.net>
2024-04-26 15:39:01 +0900

    [feat] 토큰 체크 api, model 추가

---
+++ core/data/src/main/java/com/sulsul/core/data/remote/model/request/TokenRequest.kt
@@ -0,0 +1,10 @@
+package com.sulsul.core.data.remote.model.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TokenRequest(
+    @SerialName("accessToken") val accessToken: String,
+    @SerialName("refreshToken") val refreshToken: String
+)
--- core/data/src/main/java/com/sulsul/core/data/remote/repository/LoginRepository.kt
+++ core/data/src/main/java/com/sulsul/core/data/remote/repository/LoginRepository.kt
@@ -3,6 +3,7 @@ package com.sulsul.core.data.remote.repository
 import com.sulsul.core.data.api.LoginApi
 import com.sulsul.core.data.remote.model.request.LoginRequest
 import com.sulsul.core.data.remote.model.response.LoginResponse
+import com.sulsul.core.data.remote.model.response.TokenResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
