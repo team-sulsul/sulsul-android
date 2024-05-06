14099e969dca5080cd93eaecfbdd888e0a463c68
안정은 <jjeee1313@gmail.com>
2024-05-06 19:28:05 +0900

    [chore] TokenResponse 구조 변경

--- core/data/src/main/java/com/sulsul/core/data/remote/model/response/TokenResponse.kt
+++ core/data/src/main/java/com/sulsul/core/data/remote/model/response/TokenResponse.kt
@@ -5,7 +5,15 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TokenResponse(
+    val resultCode: Long,
+    val resultMessage: String,
+    val resultData: TokenResult
+)
+
+@Serializable
+data class TokenResult(
     @SerialName("accessToken") val accessToken: String?,
     @SerialName("refreshToken") val refreshToken: String?,
     @SerialName("message") val message: String?
+
 )
