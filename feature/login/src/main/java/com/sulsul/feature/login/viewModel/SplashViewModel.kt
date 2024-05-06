ad7d0a1a58b15e17a8d3d8766fe35928c824d838
안정은 <jjeee1313@gmail.com>
2024-05-06 19:28:56 +0900

    [feat] 토큰 유효성 검사

--- feature/login/src/main/java/com/sulsul/feature/login/viewModel/SplashViewModel.kt
+++ feature/login/src/main/java/com/sulsul/feature/login/viewModel/SplashViewModel.kt
@@ -8,6 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,6 +20,8 @@ class SplashViewModel @Inject constructor(
     private val _loginState = MutableStateFlow<String>(TokenState.LOADING)
     val loginState: StateFlow<String> = _loginState
 
+    val tokenData = loginRepository.getTokenData()
+
     // 결과에 따라 이동활 화면 구분
     fun checkToken(accessToken: String, refreshToken: String) {
         viewModelScope.launch {
@@ -26,18 +29,30 @@ class SplashViewModel @Inject constructor(
                 .catch {
 
                 }.collect {
-                    when (it.message) {
-                        TokenState.TOKEN_VALID -> { // 토큰 정상
-                            _loginState.value = TokenState.TOKEN_VALID
-                        }
-                        TokenState.TOKEN_ACCESS_EXPIRED -> { // accessToken 만료
-                            _loginState.value = TokenState.TOKEN_ACCESS_EXPIRED
-                        }
-                        TokenState.TOKEN_REFRESH_EXPIRED -> { // refreshToken 만료
-                            _loginState.value = TokenState.TOKEN_REFRESH_EXPIRED
-                        }
-                        else -> {
-                            _loginState.value = TokenState.FAILURE
+                    if (it.resultCode.toInt() == 200) {
+                        when (it.resultData.message) {
+                            TokenState.TOKEN_VALID -> { // 토큰 정상
+                                _loginState.value = TokenState.TOKEN_VALID
+                            }
+
+                            TokenState.TOKEN_ACCESS_EXPIRED -> { // accessToken 만료
+                                tokenData.collect{tokenData ->
+                                    val accessToken = it.resultData.accessToken
+                                    val refreshToken = tokenData.refreshToken
+                                    if (accessToken != null) {
+                                        loginRepository.updateTokenData(accessToken, refreshToken)
+                                    }
+                                    _loginState.value = TokenState.TOKEN_ACCESS_EXPIRED
+                                }
+                            }
+
+                            TokenState.TOKEN_REFRESH_EXPIRED -> { // refreshToken 만료
+                                _loginState.value = TokenState.TOKEN_REFRESH_EXPIRED
+                            }
+
+                            else -> {
+                                _loginState.value = TokenState.FAILURE
+                            }
                         }
                     }
                 }
