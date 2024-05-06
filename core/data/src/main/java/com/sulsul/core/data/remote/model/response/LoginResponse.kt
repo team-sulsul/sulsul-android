9e5e4530c0a06601bcab7a615d766babeae67cef
안정은 <jjeee1313@gmail.com>
2024-05-06 17:20:15 +0900

    [chore] loginResponse 구조 변경

--- core/data/src/main/java/com/sulsul/core/data/remote/model/response/LoginResponse.kt
+++ core/data/src/main/java/com/sulsul/core/data/remote/model/response/LoginResponse.kt
@@ -5,9 +5,17 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class LoginResponse(
+    val resultCode: Long,
+    val resultMessage: String,
+    val resultData: LoginResult
+)
+
+@Serializable
+data class LoginResult(
     @SerialName("accessToken") val accessToken: String,
     @SerialName("refreshToken") val refreshToken: String,
     @SerialName("grantType") val grantType: String,
     @SerialName("expiresIn") val expiresIn: Long,
     @SerialName("message") val message: String
 )
+
--- feature/login/src/main/java/com/sulsul/feature/login/viewModel/LoginViewModel.kt
+++ feature/login/src/main/java/com/sulsul/feature/login/viewModel/LoginViewModel.kt
@@ -2,7 +2,7 @@ package com.sulsul.feature.login.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sulsul.core.domain.login.LoginUseCase
+import com.sulsul.core.data.remote.repository.LoginRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,10 +13,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val loginUseCase: LoginUseCase
+    private val loginRepository: LoginRepository
 ) : ViewModel() {
 
-    private val success = "200"
     private val TAG = "login"
 
     private val _loinSuccess = MutableStateFlow<Boolean>(false)
@@ -28,16 +27,16 @@ class LoginViewModel @Inject constructor(
     // TODO : 서버 측에서 응답 구조, 에러코드 상세화 변경되면 코드 변경 필요
     fun tryLogin(kakaoAccess: String) {
         viewModelScope.launch {
-            loginUseCase.tryLogin(kakaoAccess)
+            loginRepository.postLogin(kakaoAccess)
                 .catch {
                     _errorMsg.value = "failed"
                     Timber.tag(TAG).d(it)
                 }.collect {
-                    if (it.message == success) {
+                    if (it.resultCode.toInt() == 200) {
                         _loinSuccess.value = true
                     } else {
                         _errorMsg.value = "failed" // 서버에서 fail에 대한 응답을 줘야 수정할 수 있을듯..?
-                        Timber.tag(TAG).d(it.message)
+                        Timber.tag(TAG).d(it.resultMessage)
                     }
                 }
         }
