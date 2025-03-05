e4bec95c5644a3ae7700b2761518af503e8ab6dc
안정은 <jjeee1313@gmail.com>
2025-03-05 18:29:24 +0900

    [chore] ktlint format 적용

--- feature/login/src/main/java/com/sulsul/feature/login/SplashActivity.kt
+++ feature/login/src/main/java/com/sulsul/feature/login/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
